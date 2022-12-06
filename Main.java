2cbdccff68b4c009f1a281ecad8c807e927e4380
Hyperion04 <eliasmanca20@gmail.com>
2022-12-06 17:39:48 -0300

    Ejercicio

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+  public static void main(String[] args) {
+      suma(10, 30, 60);
+  }
+
+  public static void suma(int a, int b, int c) {
+      int resultado;
+      resultado = a + b;
+
+      System.out.println(resultado);
+  }
+}
