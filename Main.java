8144f0a38aef84261ce7a33ababe95dacc4c3a05
Hyperion04 <eliasmanca20@gmail.com>
2022-12-06 17:54:01 -0300

    Ejercicio 2

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main {
 
   public static void main(String[] args) {
       suma(10, 30, 60);
+
+      coche miCoche = new coche();
+      miCoche.agregarPuerta();
+      System.out.println(miCoche.puertas);
   }
 
   public static void suma(int a, int b, int c) {
@@ -9,5 +13,14 @@ public class Main {
       resultado = a + b;
 
       System.out.println(resultado);
+  }
+}
+
+
+class coche {
+  public int puertas = 0;
+
+  public void agregarPuerta() {
+    this.puertas++;
   }
 }
